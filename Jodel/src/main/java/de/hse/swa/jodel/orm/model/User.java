0c51f1f51792528903906104f34967a00cd90daf
Jay Imort <jayimort2005@gmail.com>
2022-05-17 16:09:02 +0200

    User created

--- Jodel/src/main/java/de/hse/swa/jodel/orm/model/User.java
+++ Jodel/src/main/java/de/hse/swa/jodel/orm/model/User.java
@@ -27,31 +27,56 @@ import javax.persistence.Table;
  * 
  */
 @Entity
-@Table(name = "Tuser")
+@Table(name = "User")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
     @Id
-    @SequenceGenerator(name = "tuserSeq", sequenceName = "ZSEQ_TUSER_ID", allocationSize = 1, initialValue = 10)
-    @GeneratedValue(generator = "tuserSeq")
-    
-    @Column(name = "ID")
-    private Long id;
+    @SequenceGenerator(name = "userSeq", sequenceName = "ZSEQ_USER_ID", allocationSize = 1, initialValue = 10)
+    @GeneratedValue(generator = "userSeq")
 
-    @Basic(optional=false)
-    @Column(name = "username", length=64, unique = true)
-    private String username;
-    
-    @Basic(optional=true)
-    @Column(name = "password", length=64)
-    private String password;
+	@Column(name = "USER_ID")
+	private Long id;
+
+	@Basic(optional = false)
+	@Column(name = "userFirstName",length = 50)
+	private String userFirstName;
+
+	@Basic(optional = false)
+	@Column(name = "userLastName",length = 50)
+	private String userLastName;
+
+	@Basic(optional = false)
+	@Column(name = "userLoginName",length = 50)
+	private String userLoginName;
+
+	@Basic(optional = false)
+	@Column(name = "userMail",length = 50)
+	private String userMail;
+
+	@Basic(optional = false)
+	@Column(name = "userPhoneNumber1",length = 50)
+	private String userPhoneNumber1;
+
+	@Basic(optional = true)
+	@Column(name = "userPhoneNumber2",length = 50)
+	private String userPhoneNumber2;
+	
+	@Basic(optional = false)
+	@Column(name = "isAdmin")
+	private boolean isAdmin;
 
 	public User() {
 	}
 
-	public User(String name) {
-		this.username = name;
+	public User(String name, String lastName, String loginName, String mail, String phoneNumber1, boolean isAdmin) {
+		this.userFirstName = name;
+		this.userLastName = lastName;
+		this.userLoginName = loginName;
+		this.userMail = mail;
+		this.userPhoneNumber1 = phoneNumber1;
+		this.isAdmin = isAdmin;
 	}
 
 	
@@ -63,23 +88,67 @@ public class User implements Serializable {
 		this.id = id;
 	}
 
-
-	public String getUsername() {
-		return this.username;
+	public String getFirstName() {
+		return this.userFirstName;
 	}
 
-	public void setUsername(String name) {
-		this.username = name;
+	public void setFirstname(String name) {
+		this.userFirstName = name;
 	}
 	
-
-	public String getPassword() {
-		return this.password;
+	public String getLastName() {
+		return this.userLastName;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public void setLastName(String lastName) {
+		this.userLastName = lastName;
 	}
 
+	public String getLoginName() {
+		return this.userLoginName;
+	}
+
+	public void setLoginName(String loginName) {
+		this.userLoginName = loginName;
+	}
+
+	public String getMail() {
+		return this.userMail;
+	}
+
+	public void setMail(String mail) {
+		this.userMail = mail;
+	}
+
+	public String getPhoneNumber1() {
+		return this.userPhoneNumber1;
+	}
+
+	public void setPhoneNumber1(String phoneNumber1) {
+		this.userPhoneNumber1 = phoneNumber1;
+	}
+
+	public String getPhoneNumber2() {
+		return this.userPhoneNumber2;
+	}
+
+	public void setPhoneNumber2(String phoneNumber2) {
+		this.userPhoneNumber2 = phoneNumber2;
+	}
+
+	public boolean isAdmin() {
+		return this.isAdmin;
+	}
+
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + userFirstName + ", lastName=" + userLastName + ", loginName=" + userLoginName
+				+ ", mail=" + userMail + ", phoneNumber1=" + userPhoneNumber1 + ", phoneNumber2=" + userPhoneNumber2 + ", isAdmin="
+				+ isAdmin + "]";
+	}
 	
 }
